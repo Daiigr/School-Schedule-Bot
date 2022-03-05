869902458dbd6648b3f6c066605fd26778a936a3
Daiigr <danielgurbac@gmail.com>
2022-03-05 17:28:46 +0300

    Fixed TimedEvent Return

--- schoolbot/src/main/java/com/daiigr/ScheduleHandler.java
+++ schoolbot/src/main/java/com/daiigr/ScheduleHandler.java
@@ -3,45 +3,50 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class ScheduleHandler {
-    String fileString = "timedevents.schedule";
+    private String FileName;
+
     String Output = "";
     String Day[];
     String TimedEvents[];
     String RawTimedEventData[];
     int TimedEventData[];
 
+    public ArrayList<ArrayList<TimedEvent>> TimedEventArray;
     ArrayList<TimedEvent> Temp;
-    ArrayList<ArrayList<TimedEvent>> TimedEventList;
-    
-    
 
-    public ScheduleHandler() throws IOException{
 
-        TimedEventList = new ArrayList<ArrayList<TimedEvent>>();
-        
-        File f = new File( fileString);
+    public ScheduleHandler(String FileName) throws Exception{
+
+        TimedEventArray = new ArrayList<ArrayList<TimedEvent>>();
+
+
+        this.FileName = FileName;
+        File f = new File( FileName);
         if(!f.exists()){
             f.createNewFile();
         }
-
-        FileReader fr = new FileReader(fileString);
+        FileReader fr = new FileReader(FileName);
         int i;    
         while((i=fr.read())!=-1){
             Output += (char)i;  
         }
         fr.close();
 
-        //removes all line breaks
         Output = Output.replace("\r\n", "").replace("\n", "");
+
         //splits string into a array splitting from & representing individual timedEvent Arrays
         Day = Output.split("&");
 
         for(int j = 0; j < Day.length; j++ ){
+
                     //splits string into a array splitting from % representing individual timedEvents
 
             TimedEvents = Day[j].split("%");
+            Temp = new ArrayList<TimedEvent>();
 
             for(int x = 1; x < TimedEvents.length; x++){
+
+                
 
                 //splits string into a array splitting from : representing individual elements of timedEvents
                 RawTimedEventData = TimedEvents[x].split(":");
@@ -49,21 +54,28 @@ public class ScheduleHandler {
                 TimedEventData = new int[RawTimedEventData.length];
 
                 for(int p = 0; p < RawTimedEventData.length; p++){
-                    //TimedEventData[p] = Integer.parseInt(RawTimedEventData[p]);
+
+                    TimedEventData[p] = Integer.parseInt(RawTimedEventData[p]);
                 
                 }
-                System.out.println("Set: "+ j + " ID: " + RawTimedEventData[0] + " Hour: " + RawTimedEventData[1] + " minute: "  + RawTimedEventData[2]);
-                Temp = new ArrayList<TimedEvent>();
-               Temp.add(new TimedEvent(TimedEventData[0],TimedEventData[1],TimedEventData[2]));
+               Temp.add(new TimedEvent(TimedEventData[0], TimedEventData[1], TimedEventData[2]));
+               
+            
+               //  System.out.println("Set: "+ j + " ID: " + RawTimedEventData[0] + " Hour: " + RawTimedEventData[1] + " minute: "  + RawTimedEventData[2]);
 
             }
 
-            TimedEventList.add(Temp);
+            TimedEventArray.add(Temp);
+
 
         }
+  
+    }
 
     
-    }
+
+
+
     
 
    
